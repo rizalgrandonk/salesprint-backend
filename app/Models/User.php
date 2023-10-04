4ac67f3be05174fa32c65b986a2bb47f603b0c09
rizalgrandonk <grandonk457@gmail.com>
2023-10-04 13:38:09 +0700

    update auto cretae model config

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,9 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 /**
  * Class User
@@ -37,8 +40,9 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @package App\Models
  */
-class User extends Model
-{
+class User extends Authenticatable implements JWTSubject {
+	use Notifiable;
+
 	protected $table = 'users';
 
 	protected $casts = [
@@ -67,18 +71,33 @@ class User extends Model
 		'remember_token'
 	];
 
-	public function orders()
-	{
+	public function orders() {
 		return $this->hasMany(Order::class);
 	}
 
-	public function reviews()
-	{
+	public function reviews() {
 		return $this->hasMany(Review::class);
 	}
 
-	public function stores()
-	{
+	public function stores() {
 		return $this->hasMany(Store::class);
+	}
+
+	/**
+	 * Get the identifier that will be stored in the subject claim of the JWT.
+	 *
+	 * @return mixed
+	 */
+	public function getJWTIdentifier() {
+		return $this->getKey();
+	}
+
+	/**
+	 * Return a key value array, containing any custom claims to be added to the JWT.
+	 *
+	 * @return array
+	 */
+	public function getJWTCustomClaims() {
+		return [];
 	}
 }
